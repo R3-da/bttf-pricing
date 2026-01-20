5dceac8cacc16677ffc59df41218c45b6d7dd00f
reda haddan <haddan.reda.net@gmail.com>
2026-01-20 15:23:52 +0100

    Initialize FastAPI project with basic structure and dependencies
    (cherry picked from commit 61dc83d10fc9d42397eecac70508f8976805e48f)

---
+++ src/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+from src.api.endpoints import router as pricing_router
+
+app = FastAPI(
+    title="Back to the Future Pricing API",
+    description="API to calculate the price of Back to the Future DVDs with special discounts.",
+    version="0.1.0",
+)
+
+from fastapi.staticfiles import StaticFiles
+
+app.include_router(pricing_router, prefix="/api/v1")
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
