446a29297415c0f928d518ce3c9ef014496295a6
reda haddan <haddan.reda.net@gmail.com>
2026-01-20 16:15:01 +0100

    Add unit tests for BackToTheFutureStrategy pricing calculations
    (cherry picked from commit 772ad2daa3b3cbfd1c966b210b886f3d118ad3e5)

---
+++ tests/test_pricing.py
@@ -0,0 +1,37 @@
+import pytest
+from src.domain.models import Cart
+from src.core.strategies import BackToTheFutureStrategy
+
+@pytest.fixture
+def strategy():
+    return BackToTheFutureStrategy()
+
+def test_example_1(strategy):
+    # Back to the Future 1 Back to the Future 2 Back to the Future 3 -> 36
+    # 3 distinct BTTF -> 20% off all BTTF. (15*3)*0.8 = 36
+    cart = Cart.from_text("Back to the Future 1\nBack to the Future 2\nBack to the Future 3")
+    assert strategy.calculate_price(cart) == 36
+
+def test_example_2(strategy):
+    # Back to the Future 1 Back to the Future 3 -> 27
+    # 2 distinct BTTF -> 10% off all BTTF. (15*2)*0.9 = 27
+    cart = Cart.from_text("Back to the Future 1\nBack to the Future 3")
+    assert strategy.calculate_price(cart) == 27
+
+def test_example_3(strategy):
+    # Back to the Future 1 -> 15
+    # 1 distinct BTTF -> 0% off. 15
+    cart = Cart.from_text("Back to the Future 1")
+    assert strategy.calculate_price(cart) == 15
+
+def test_example_4(strategy):
+    # Back to the Future 1 Back to the Future 2 Back to the Future 3 Back to the Future 2 -> 48
+    # 3 distinct BTTF (1, 2, 3) -> 20% off ALL BTTF. (15*4)*0.8 = 48
+    cart = Cart.from_text("Back to the Future 1\nBack to the Future 2\nBack to the Future 3\nBack to the Future 2")
+    assert strategy.calculate_price(cart) == 48
+
+def test_example_5(strategy):
+    # Back to the Future 1 Back to the Future 2 Back to the Future 3 La chèvre -> 56
+    # 3 distinct BTTF -> 20% off ALL BTTF. ((15*3)*0.8) + 20 = 45*0.8 + 20 = 36+20 = 56
+    cart = Cart.from_text("Back to the Future 1\nBack to the Future 2\nBack to the Future 3\nLa chèvre")
+    assert strategy.calculate_price(cart) == 56
